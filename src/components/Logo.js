d667626223fb2bbb7dfc06195af7e5bf6362c70c
iamalexstanciu <alexstanciu@outlook.it>
2023-10-03 17:55:53 +0300

    Working on logo.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import LogoHeader from "../assets/images/logo.png";
 
 const Container = styled.div`
   position: absolute;
   top: 1rem;
   left: 1rem;
 
-  width: 100%;
-  color: ${(props) => props.theme.text};
+  width: 50%;
   z-index: 5;
 `;
 
 const Logo = () => {
-  return <Container>Logo</Container>;
+  return (
+    <Container>
+      <Link to="/">
+        <svg xmlns="" />
+      </Link>
+    </Container>
+  );
 };
 
 export default Logo;
