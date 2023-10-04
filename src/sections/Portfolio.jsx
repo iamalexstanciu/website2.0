d506c582d3a549ba7a75e66d4a226e6981ff9b55
iamalexstanciu <alexstanciu@outlook.it>
2023-10-04 18:35:06 +0300

    Not working correctly.

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -152,7 +152,7 @@ const Item = styled.div`
 const Photos = ({ img, name }) => {
   return (
     <Item>
-      <img style={{ width: "400", height: "600" }} src={img} alt={name} />
+      <img src={img} alt={name} />
       <h2>{name}</h2>
     </Item>
   );
