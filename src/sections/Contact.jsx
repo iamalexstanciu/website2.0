d1633bcf76f2e01d13b4df24d344d214359e6d91
iamalexstanciu <alexstanciu@outlook.it>
2023-10-18 16:46:12 +0300

    Working on contact section.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,22 +5,22 @@ function Contact() {
   return (
     <div className="section-contact" id="contact">
       <div className="container-contact">
-        <h1 className="title">Get in Touch!</h1>
+        <h1 className="title-contact">Get in Touch!</h1>
         <div className="details-contact">
           <a className="email-address" href="mailto:info@upvisionmedia.com">
-            info@upvisionmedia.com
+            email us
           </a>
-          <a href="tel:+40744497740">Call us!</a>
+          <a href="tel:+40744497740">call us</a>
           <div className="mail-to-owners">
             <a
               className="email-address-alex"
               href="mailto:a.stanciu@upvisionmedia.com">
-              a.stanciu@upvisionmedia.com
+              Alex
             </a>
             <a
               className="email-address-razvan"
               href="mailto:r.tache@upvisionmedia.com">
-              r.tache@upvisionmedia.com
+              Razvan
             </a>
           </div>
           <div className="other-emails">
