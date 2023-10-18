169eb214d58993039849e771e44d19cb554698fc
iamalexstanciu <alexstanciu@outlook.it>
2023-10-18 11:13:30 +0300

    Changed design services page.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../style/services.css";
-import Video from "../assets/video/smoke-orange.mp4"
+import Video from "../assets/video/smoke-orange.mp4";
 
 const Services = () => {
   return (
@@ -10,168 +10,161 @@ const Services = () => {
         data-scroll
         data-scroll-speed="2"
         data-scroll-direction="vertical">
-        <div className="left-side-services">
+        <div className="title-container">
           <h1
             className="title-services"
             data-scroll
-            data-scroll-speed="-2"
+            data-scroll-speed="1"
             data-scroll-direction="horizontal">
             Services
           </h1>
-          <div className="content-services">
-            Website Development: At our web development agency, we specialize in
-            creating stunning and highly functional websites tailored to your
-            unique needs. Our expert team of designers and developers work
-            closely with you to bring your vision to life. We focus on
-            user-friendly interfaces, intuitive navigation, and captivating
-            aesthetics to ensure your website not only looks great but also
-            delivers an exceptional user experience.
-            <br />
-            <br />
-            SEO Optimization: We understand that having a beautiful website is
-            just the first step. To ensure your online presence is impactful, we
-            provide comprehensive SEO optimization services. Our SEO experts
-            employ the latest strategies and techniques to improve your
-            website's visibility on search engines. This includes keyword
-            research, on-page and off-page optimization, content optimization,
-            and more. With our SEO services, your website will not only be
-            visually appealing but also rank higher in search engine results,
-            driving more organic traffic and potential customers to your site.
-            <br />
-            <br />
-            Mobile Responsiveness: In today's mobile-centric world, having a
-            mobile-responsive website is crucial. Our agency ensures that your
-            website is fully optimized for mobile devices, providing a seamless
-            and visually pleasing experience on smartphones and tablets. This
-            mobile responsiveness not only enhances user satisfaction but also
-            improves your website's search engine rankings, as
-            mobile-friendliness is a key factor in search algorithms.
-          </div>
         </div>
-        <div
-          class="features-1 widget-services-1"
-          data-scroll
-          data-scroll-speed="-2"
-          data-scroll-direction="vertical">
-          <svg
-            class="rectangle-10"
-            width="263"
-            height="188"
-            viewBox="0 0 263 188"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-              fill="#000000"
-              fill-opacity="0.9"
-            />
-          </svg>
 
-          <img
-            class="image-1"
-            src="https://images.unsplash.com/photo-1657812160299-6b656decd5b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80"
-          />
-          <svg
-            class="frame-13"
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-            <path
-              d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-              fill="black"
-            />
-          </svg>
-
-          <div class="title-card">Search Engine Optimization</div>
-          <div class="frame-14">
-            <div class="read-more">Read More</div>
+        <div className="content-services">
+          <div className="top-services">
+            <div className="content-services">
+              An SEO-optimized website enhances online visibility, attracts more
+              organic traffic, and drives business growth by reaching a broader
+              audience and outperforming competitors.
+              <br />
+              <br />
+              Mobile responsiveness is crucial as it ensures a seamless user
+              experience on smartphones and tablets, enhances SEO, and expands
+              your reach to a vast mobile audience, boosting website performance
+              and success.
+              <br />
+              <br />A website is your 24/7 digital storefront, essential for
+              reaching and engaging customers, establishing credibility, and
+              expanding your business in today's online-driven world.
+            </div>
           </div>
-        </div>
-        <div
-          class="features-1 widget-services-2"
-          data-scroll
-          data-scroll-speed="-1"
-          data-scroll-direction="vertical">
-          <svg
-            class="rectangle-10"
-            width="263"
-            height="188"
-            viewBox="0 0 263 188"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-              fill="#333333"
-              fill-opacity="0.9"
-            />
-          </svg>
+          <div className="widgets-services">
+            <div
+              class="features-1 widget-services-1"
+              data-scroll
+              data-scroll-speed="-2"
+              data-scroll-direction="vertical">
+              <svg
+                class="rectangle-10"
+                width="263"
+                height="188"
+                viewBox="0 0 263 188"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
+                  fill="#000000"
+                  fill-opacity="0.9"
+                />
+              </svg>
 
-          <img
-            class="image-1"
-            src="https://images.unsplash.com/photo-1687727739807-08a19b70a664?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-          />
-          <svg
-            class="frame-13"
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-            <path
-              d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-              fill="black"
-            />
-          </svg>
+              <img
+                class="image-1"
+                src="https://images.unsplash.com/photo-1657812160299-6b656decd5b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80"
+              />
+              <svg
+                class="frame-13"
+                width="33"
+                height="33"
+                viewBox="0 0 33 33"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
+                <path
+                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
+                  fill="black"
+                />
+              </svg>
 
-          <div class="title-card">Mobile Responsive</div>
-          <div class="frame-14">
-            <div class="read-more">Read More</div>
-          </div>
-        </div>
-        <div
-          class="features-1 widget-services-3"
-          data-scroll
-          data-scroll-speed="-2"
-          data-scroll-direction="vertical">
-          <svg
-            class="rectangle-10"
-            width="263"
-            height="188"
-            viewBox="0 0 263 188"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-              fill="#b3b3b3"
-              fill-opacity="0.9"
-            />
-          </svg>
+              <div class="title-card">Search Engine Optimization</div>
+              <div class="frame-14">
+                <div class="read-more">Read More</div>
+              </div>
+            </div>
+            <div
+              class="features-1 widget-services-2"
+              data-scroll
+              data-scroll-speed="-2"
+              data-scroll-direction="vertical">
+              <svg
+                class="rectangle-10"
+                width="263"
+                height="188"
+                viewBox="0 0 263 188"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
+                  fill="#333333"
+                  fill-opacity="0.9"
+                />
+              </svg>
 
-          <img
-            class="image-1"
-            src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-          />
-          <svg
-            class="frame-13"
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-            <path
-              d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-              fill="black"
-            />
-          </svg>
+              <img
+                class="image-1"
+                src="https://images.unsplash.com/photo-1687727739807-08a19b70a664?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              />
+              <svg
+                class="frame-13"
+                width="33"
+                height="33"
+                viewBox="0 0 33 33"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
+                <path
+                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
+                  fill="black"
+                />
+              </svg>
 
-          <div class="title-card">Website Development</div>
-          <div class="frame-14">
-            <div class="read-more">Read More</div>
+              <div class="title-card">Mobile Responsive</div>
+              <div class="frame-14">
+                <div class="read-more">Read More</div>
+              </div>
+            </div>
+            <div
+              class="features-1 widget-services-3"
+              data-scroll
+              data-scroll-speed="-2"
+              data-scroll-direction="vertical">
+              <svg
+                class="rectangle-10"
+                width="263"
+                height="188"
+                viewBox="0 0 263 188"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
+                  fill="#b3b3b3"
+                  fill-opacity="0.9"
+                />
+              </svg>
+
+              <img
+                class="image-1"
+                src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              />
+              <svg
+                class="frame-13"
+                width="33"
+                height="33"
+                viewBox="0 0 33 33"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
+                <path
+                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
+                  fill="black"
+                />
+              </svg>
+
+              <div class="title-card">Website Development</div>
+              <div class="frame-14">
+                <div class="read-more">Read More</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
