7b01ae84231f0af10aef1cbbe619bc6bfa74a179
iamalexstanciu <alexstanciu@outlook.it>
2023-10-19 10:10:13 +0300

    Portfolio structure done.

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,15 +1,6 @@
 import "../style/portfolio.css";
 import React, { useEffect, useRef } from "react";
 
-import Img1 from "../assets/images/project1.jpg";
-import Img2 from "../assets/images/project1.1.jpg";
-import Img3 from "../assets/images/project2.jpg";
-import Img4 from "../assets/images/project2.1.jpg";
-import Img5 from "../assets/images/project3.jpg";
-import Img6 from "../assets/images/project3.1.jpg";
-import Img7 from "../assets/images/project4.png";
-import Img8 from "../assets/images/project4.1.png";
-
 const Portfolio = () => {
   return (
     <div className="section-portfolio" id="portfolio">
@@ -18,138 +9,52 @@ const Portfolio = () => {
         data-scroll
         data-scroll-speed="2"
         data-scroll-direction="vertical">
-        <div className="left-side-portfolio">
-          <h1
-            className="section-name-portfolio"
-            data-scroll
-            data-scroll-speed="-1"
-            data-scroll-direction="horizontal">
-            Portfolio
-          </h1>
-
-          <div
-            className="content-portfolio"
-            data-scroll
-            data-scroll-speed="-2"
-            data-scroll-direction="vertical">
-            <p>
-              Welcome to our Portfolio Showcase, where we proudly present a
-              curated selection of our finest web development projects. Each
-              project is a testament to our team's creativity, technical
-              expertise, and unwavering commitment to excellence.
-              <br />
-              <br />
-              Explore a diverse range of visually stunning and functionally
-              exceptional websites that we've crafted for clients across various
-              industries. From sleek and modern designs to user-friendly
-              e-commerce platforms, our portfolio embodies the essence of
-              innovation and craftsmanship.
-              <br />
-              <br />
-              These projects are not just websites; they are successful digital
-              experiences that have helped our clients thrive online. Discover
-              the possibilities with our web development agency through our
-              showcased masterpieces.
+        <h1
+          className="section-name-portfolio"
+          data-scroll
+          data-scroll-speed="-1"
+          data-scroll-direction="horizontal">
+          Portfolio
+        </h1>
+        <div className="container-projects">
+          <div className="project-widget project1">
+            <h1 className="title-project">Confort Uno Mobili</h1>
+            <p className="short-description">
+              Interior design website developed.
             </p>
+            <h4 className="technologies-used">react, js, css</h4>
           </div>
-        </div>
-        <div class="external">
-          <div class="horizontal-scroll-wrapper">
-            <div class="img-wrapper slower">
-              <a href="#0" target="_blank" rel="noreferrer">
-                <img src={Img1} alt="" />
-              </a>
-            </div>
 
-            <div class="img-wrapper faster">
-              <a href="#0" target="_blank" rel="noreferrer">
-                <img src={Img2} alt="" />
-              </a>
-            </div>
+          <div className="project-widget project2">
+            <h1 className="title-project">AFA Trasporti</h1>
+            <p className="short-description">
+              Presentation website for a transport company.
+            </p>
+            <h4 className="technologies-used">react, js, css</h4>
+          </div>
 
-            <div class="img-wrapper slower vertical">
-              <a href="#0" target="_blank" rel="noopener">
-                <img src={Img3} alt="" />
-              </a>
-            </div>
+          <div className="project-widget project3">
+            <h1 className="title-project">Portfolio AlexStanciu</h1>
+            <p className="short-description">
+              Designed and developed a personal portfolio.
+            </p>
+            <h4 className="technologies-used">react, js, css</h4>
+          </div>
 
-            <div class="img-wrapper slower slower-down">
-              <a href="#0" target="_blank" rel="noopener">
-                <img src={Img4} alt="" />
-              </a>
-            </div>
+          <div className="project-widget project4">
+            <h1 className="title-project">Dr. Catalin Teodor</h1>
+            <p className="short-description">
+              Interesting project for a dentist website.
+            </p>
+            <h4 className="technologies-used">react, parallax, js, css</h4>
+          </div>
 
-            <div class="img-wrapper">
-              <a
-                href="https://altphotos.com/photo/sidewalk-terrace-of-a-blue-facade-cafe-312/"
-                target="_blank"
-                rel="noreferrer">
-                <img src={Img5} alt="" />
-              </a>
-            </div>
-
-            <div class="img-wrapper slower">
-              <a
-                href="https://altphotos.com/photo/paris-waterfront-at-sunset-1555/"
-                target="_blank"
-                rel="noreferrer">
-                <img src={Img6} alt="" />
-              </a>
-            </div>
-
-            <div class="img-wrapper faster1">
-              <a
-                href="https://altphotos.com/photo/old-man-leaning-over-the-barrier-looking-at-the-river-265/"
-                target="_blank"
-                rel="noreferrer">
-                <img src={Img7} alt="" />
-              </a>
-            </div>
-
-            <div class="img-wrapper slower slower2">
-              <a
-                href="https://altphotos.com/photo/cafe-terrace-with-a-row-of-retro-tables-261/"
-                target="_blank"
-                rel="noreferrer">
-                <img src={Img8} alt="" />
-              </a>
-            </div>
-
-            <div class="img-wrapper">
-              <a
-                href="https://altphotos.com/photo/street-scene-with-pedestrians-and-dogs-318/"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/street-scene-people.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-
-            <div class="img-wrapper slower">
-              <a
-                href="https://altphotos.com/photo/tourist-barge-on-the-river-seine-near-notre-dame-266/"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/notre-dame-river-boat.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-
-            <div class="img-wrapper slower last">
-              <a
-                href="https://altphotos.com/photo/skulls-decoration-in-a-shop-window-331/"
-                target="_blank"
-                rel="noreferrer">
-                <img
-                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/shop-window-reflection.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
+          <div className="project-widget project5">
+            <h1 className="title-project">upvisionMedia 1.0</h1>
+            <p className="short-description">
+              First version of upvisionMedia website.
+            </p>
+            <h4 className="technologies-used">react, js, css</h4>
           </div>
         </div>
       </div>
