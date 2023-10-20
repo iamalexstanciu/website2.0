2041209137c175ba531ca64326312a0929f8978b
iamalexstanciu <alexstanciu@outlook.it>
2023-10-20 11:21:37 +0300

    Added new design home button.

--- src/components/ButtonStartAProject.js
+++ src/components/ButtonStartAProject.js
@@ -6,46 +6,36 @@ const ButtonSAP = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  top: 85%;
+  top: 70%;
   z-index: 200;
-  width: 15%;
-  left: calc(90% - 130px);
+  width: fit-content;
+  left: calc(75% - 130px);
 
-  .btn {
-    line-height: 1;
+  .btn-home {
+    border: none !important;
     padding: 20px 30px;
-    background-color: #0000;
-    border-radius: 200px;
-    font-size: 18px;
-    outline: none;
+    font-size: 26px;
+    background-color: transparent;
     color: #fc4308;
     opacity: 1;
     z-index: 200;
     transform: scale(1);
-    animation: pulse 2s infinite;
     text-transform: lowercase;
   }
 
-  @keyframes pulse {
-    0% {
-      transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
-    }
-
-    70% {
-      transform: scale(1);
-      box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
-    }
-
-    100% {
-      transform: scale(0.95);
-      box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  .btn-home:hover {
+    .arrow-button-home {
+      transform: rotate(45deg);
+      transition: 300ms linear all;
+      position: relative;
+      top: 3rem;
+      left: 5%;
     }
   }
 `;
 
 const ButtonStartAProject = () => {
-    const { scroll } = useLocomotiveScroll();
+  const { scroll } = useLocomotiveScroll();
   const handleScroll = (id) => {
     let elem = document.querySelector(id);
 
@@ -58,8 +48,33 @@ const ButtonStartAProject = () => {
 
   return (
     <ButtonSAP>
-      <button class="btn" onClick={() => handleScroll("#contact")}>
-        Start a Project
+      <button class="btn-home" onClick={() => handleScroll("#contact")}>
+        Let's Build your Future on Web{" "}
+        <svg
+          className="arrow-button-home"
+          width="126px"
+          height="126px"
+          viewBox="2 0 30 5"
+          fill="black"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#ffffff">
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke="#CCCCCC"
+            stroke-width="0.192"></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M7 17L17 7M17 7H8M17 7V16"
+              stroke="#ffffff"
+              stroke-width="1.344"
+              stroke-linecap="round"
+              stroke-linejoin="round"></path>{" "}
+          </g>
+        </svg>
       </button>
     </ButtonSAP>
   );
