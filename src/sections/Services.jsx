24e465733171a55a8ad1d6f1ec6dd6ec0ffdfcdd
iamalexstanciu <alexstanciu@outlook.it>
2023-10-20 16:37:42 +0300

    Redesign services.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -9,165 +9,28 @@ const Services = () => {
         data-scroll
         data-scroll-speed="2"
         data-scroll-direction="vertical">
-        <div className="title-container">
-          <h1
-            className="title-services"
-            data-scroll
-            data-scroll-speed="1"
-            data-scroll-direction="horizontal">
-            Services
-          </h1>
-        </div>
+        <h1
+          className="title-services"
+          data-scroll
+          data-scroll-speed="1"
+          data-scroll-direction="horizontal">
+          Services
+        </h1>
 
         <div className="content-services">
-          <div className="top-services">
-            <div className="content-services">
-              An SEO-optimized website enhances online visibility, attracts more
-              organic traffic, and drives business growth by reaching a broader
-              audience and outperforming competitors.
-              <br />
-              <br />
-              Mobile responsiveness is crucial as it ensures a seamless user
-              experience on smartphones and tablets, enhances SEO, and expands
-              your reach to a vast mobile audience, boosting website performance
-              and success.
-              <br />
-              <br />A website is your 24/7 digital storefront, essential for
-              reaching and engaging customers, establishing credibility, and
-              expanding your business in today's online-driven world.
-            </div>
+          <div className="card-services first-card-services">
+            <p className="number-card-service">01</p>
+            <h3 className="title-card-service">Responsive Design</h3>
+            <p className="description-card-service">
+              Responsive design is crucial for websites as it ensures optimal
+              user experience on various devices, improves SEO, and boosts user
+              engagement, vital for online success in today's multi-platform
+              digital landscape.
+            </p>
           </div>
-          <div className="widgets-services">
-            <div
-              class="features-1 widget-services-1"
-              data-scroll
-              data-scroll-speed="-2"
-              data-scroll-direction="vertical">
-              <svg
-                class="rectangle-10"
-                width="263"
-                height="188"
-                viewBox="0 0 263 188"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-                  fill="#fc4308"
-                  fill-opacity="0.9"
-                />
-              </svg>
-
-              <img
-                class="image-1"
-                src="https://images.pexels.com/photos/4008335/pexels-photo-4008335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="service presentation upvisionmedia"
-              />
-              <svg
-                class="frame-13"
-                width="33"
-                height="33"
-                viewBox="0 0 33 33"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-                <path
-                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-                  fill="black"
-                />
-              </svg>
-
-              <div class="title-card">Search Engine Optimization</div>
-              <div class="frame-14">
-                <div class="read-more">Read More</div>
-              </div>
-            </div>
-            <div
-              class="features-1 widget-services-2"
-              data-scroll
-              data-scroll-speed="-2"
-              data-scroll-direction="vertical">
-              <svg
-                class="rectangle-10"
-                width="263"
-                height="188"
-                viewBox="0 0 263 188"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-                  fill="#fc4308"
-                  fill-opacity="0.9"
-                />
-              </svg>
-
-              <img
-                class="image-1"
-                src="https://images.pexels.com/photos/4295841/pexels-photo-4295841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="service presentation upvisionmedia"
-              />
-              <svg
-                class="frame-13"
-                width="33"
-                height="33"
-                viewBox="0 0 33 33"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-                <path
-                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-                  fill="black"
-                />
-              </svg>
-
-              <div class="title-card">Mobile Responsive</div>
-              <div class="frame-14">
-                <div class="read-more">Read More</div>
-              </div>
-            </div>
-            <div
-              class="features-1 widget-services-3"
-              data-scroll
-              data-scroll-speed="-2"
-              data-scroll-direction="vertical">
-              <svg
-                class="rectangle-10"
-                width="263"
-                height="188"
-                viewBox="0 0 263 188"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0 21.5C0 9.62588 9.62588 0 21.5 0H29.25C39.3292 0 47.5 8.1708 47.5 18.25C47.5 28.3292 55.6708 36.5 65.75 36.5H233C249.569 36.5 263 49.9315 263 66.5V158C263 174.569 249.569 188 233 188H30C13.4315 188 0 174.569 0 158V21.5Z"
-                  fill="#fc4308"
-                  fill-opacity="0.9"
-                />
-              </svg>
-
-              <img
-                class="image-1"
-                src="https://images.unsplash.com/photo-1510125964506-dc5a14f3ef4e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="service presentation upvisionmedia"
-              />
-              <svg
-                class="frame-13"
-                width="33"
-                height="33"
-                viewBox="0 0 33 33"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <circle cx="16.5" cy="16.5" r="16.5" fill="white" />
-                <path
-                  d="M12.375 16.5344L12.3799 15.7559L12.3799 15.7559L12.375 16.5344ZM24.863 17.0849C25.1463 16.7809 25.1493 16.2879 24.8699 15.9839L20.3166 11.0295C20.0372 10.7255 19.5811 10.7255 19.2979 11.0295C19.0147 11.3335 19.0116 11.8264 19.291 12.1305L23.3384 16.5344L19.236 20.9383C18.9528 21.2424 18.9497 21.7353 19.2291 22.0393C19.5086 22.3434 19.9647 22.3434 20.2479 22.0393L24.863 17.0849ZM7.85225 17.3129L12.3701 17.3129L12.3799 15.7559L7.86198 15.7559L7.85225 17.3129ZM12.3701 17.3129L24.3523 17.3129L24.362 15.7559L12.3799 15.7559L12.3701 17.3129Z"
-                  fill="black"
-                />
-              </svg>
-
-              <div class="title-card">Website Development</div>
-              <div class="frame-14">
-                <div class="read-more">Read More</div>
-              </div>
-            </div>
-          </div>
+          <div className="card-services second-card-services"></div>
+          <div className="card-services third-card-services"></div>
+          <div className="card-services fourth-card-services"></div>
         </div>
       </div>
     </div>
