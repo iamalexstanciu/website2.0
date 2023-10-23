40a2eacae151af35e6fdee47931b4590e57ece1c
iamalexstanciu <alexstanciu@outlook.it>
2023-10-23 09:12:34 +0300

    Added color font changeble wuith theme.

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -11,19 +11,23 @@ const GlobalStyle = createGlobalStyle`
 body{
     font-family:  "Rajdhani";
     overflow-x: hidden;
-    background-color: ${(props) => (props.theme === "dark" ? "#000" : "#fff")};
+    background-color: ${(props) => (props.theme === "dark" ? "#fff" : "#000")};
+    color: ${(props) => (props.theme === "dark" ? "#000" : "#fff")};
     transition: background-color 0.5s, color 0.5s;
 }
 
 
-h1, h2, h3, h4, h5, h6{
+h1, h2, h3, h4, h5, h6, p{
     margin: 0;
     padding: 0;
+    color: ${(props) => (props.theme === "dark" ? "#000" : "#fff")};
 }
 
 a{
     color: inherit;
     text-decoration: none;
+    
+    color: ${(props) => (props.theme === "dark" ? "#000" : "#fff")};
 }
 
  `;
