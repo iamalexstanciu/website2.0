6a097c7f55f3b8dc17030777d08dfec53504ef66
iamalexstanciu <alexstanciu@outlook.it>
2023-11-02 18:50:34 +0200

    No more owners email

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -11,18 +11,6 @@ function Contact() {
             let's start a project
           </a>
           <a href="tel:+40744497740">call us</a>
-          <div className="mail-to-owners">
-            <a
-              className="email-address-alex"
-              href="mailto:a.stanciu@upvisionmedia.com">
-              Alex
-            </a>
-            <a
-              className="email-address-razvan"
-              href="mailto:r.tache@upvisionmedia.com">
-              Razvan
-            </a>
-          </div>
           <div className="other-emails">
             <a
               className="email-address-razvan"
