6cedc9fbc6a3e908a77a3051abef8851cb662cdf
iamalexstanciu <alexstanciu@outlook.it>
2024-03-11 08:47:14 +0200

    Link Calendly added.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,19 +7,26 @@ function Contact() {
       <div className="container-contact">
         <h1 className="title-contact">Get in Touch!</h1>
         <div className="details-contact">
-          <a className="email-address" href="mailto:info@upvisionmedia.com">
+          <a
+            className="email-address"
+            href="https://calendly.com/upvisionmedia/first-call"
+          >
             let's start a project
           </a>
-          <a className="phone-number" href="tel:+40744497740">call us</a>
+          <a className="phone-number" href="tel:+40744497740">
+            call us
+          </a>
           <div className="other-emails">
             <a
               className="email-address-support"
-              href="mailto:support@upvisionmedia.com">
+              href="mailto:support@upvisionmedia.com"
+            >
               Support
             </a>
             <a
               className="email-address-billing"
-              href="mailto:billing@upvisionmedia.com">
+              href="mailto:billing@upvisionmedia.com"
+            >
               Billing
             </a>
           </div>
